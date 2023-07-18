18fcc1a505fd0e6962900c43c85a7f72546308e4
EgorT <terrragoga@gmail.com>
2023-07-18 21:55:32 +0500

    Домашнее задание

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// Task 1
+
+// Console.Write("Введите первое целое число: ");
+// int numa = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе целое число: ");
+// int numb = Convert.ToInt32(Console.ReadLine());
+// if(numa > numb) Console.WriteLine(numa + " больше.");
+// else Console.WriteLine(numb + " больше.");
+
+// Task 2
+
+// Console.Write("Введите первое целое число: ");
+// int numa = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе целое число: ");
+// int numb = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите третье целое число: ");
+// int numc = Convert.ToInt32(Console.ReadLine());
+
+// int maxim = Math.Max(Math.Max(numa, numb), numc); // Искал как сократить, нашел этот метод. 
+// Console.WriteLine(maxim);
+
+// Task 3
+
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if ((num % 2) == 0) Console.WriteLine("Оно четное!");
+// else Console.WriteLine("Оно нечетное(");
+
+// Task 4
+
+//Console.Write("Введите число: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//int chetn=1;
+//while(chetn <= num)
+//{
+//    if (chetn % 2 == 0) 
+//    Console.Write(chetn + " ");
+//chetn++;
+//}
+
